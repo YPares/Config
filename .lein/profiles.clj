a0c95b4924979d378941e681dac41e15afe1a607
YPares <yves.pares@gmail.com>
2014-04-25 10:50:23 +0200

    Added cider-nrepl to lein profiles

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,2 +1,4 @@
 {:user {:plugins
-            [[lein-tarsier "0.9.4"]]}}
+            [[lein-tarsier "0.9.4"]
+             [cider/cider-nrepl "0.6.1-SNAPSHOT"]]
+        :dependencies [[com.cemerick/pomegranate "0.3.0"]]}}
