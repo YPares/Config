7c02280ab872387e2bf9b25236e3a22182586ae2
YPares <yves.pares@gmail.com>
2016-05-02 23:27:15 +0200

    Arduino makefile not system-installed

--- .makefiles/Arduino-check-syntax.mk
+++ .makefiles/Arduino-check-syntax.mk
@@ -2,7 +2,7 @@ ARDUINO_DIR = /opt/arduino
 BOARD_TAG = diecimila
 BOARD_SUB = atmega328
 
-include /usr/local/share/arduino/Arduino.mk
+include /Data/Dev/Arduino-Makefile/Arduino.mk
 
 check-syntax:
 	$(CXX) -c -include Arduino.h -x c++ $(CXXFLAGS) $(CPPFLAGS) -fsyntax-only $(CHK_SOURCES)
